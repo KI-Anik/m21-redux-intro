58900672fbc4f026ddf0762a2f44d20f5a01d404
KI-Anik <iamkhairul101@gmail.com>
2025-06-26 09:02:38 +0600

    feat : implement redux store and hook for counter functionality

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,9 @@ import counterReducer from './features/counter/counterSlice'
 
 export const store = configureStore({
     reducer: {
-        counter : counterReducer,
+        counter: counterReducer,
     }
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type appDispatch = typeof store.dispatch
